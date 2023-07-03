9ab889b3564b37f10f0a930392c26c5370759435
Alexey Anikevich <alexeyanikevich@iMac-Alexey.local>
2023-07-03 22:01:14 +0300

    V0

---
+++ news/pytest_tests/test_routes.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+import pytest as pytest
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+@pytest.mark.parametrize(
+    'name',
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
