2be4eb36db157e7578914891331eae3879cd4413
Alexey Anikevich <alexeyanikevich@iMac-Alexey.local>
2023-07-03 22:44:35 +0300

    V0

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -4,11 +4,26 @@ import pytest as pytest
 from django.urls import reverse
 from pytest_django.asserts import assertRedirects
 
+
 @pytest.mark.parametrize(
     'name',
-    ('news:home', 'users:login', 'users:logout', 'users:signup')
+    ('users:login', 'users:logout', 'users:signup')
 )
-def test_pages_availability_for_anonymous_user(client, name):
+def test_auth_pages_availability_for_anonymous_user(client, name):
     url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+def test_home_page_availability_for_anonymous_user(client):
+    url = reverse('news:home', )
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+def test_detail_news(client, news):
+    url = reverse('news:detail', args=(news.id,))
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
